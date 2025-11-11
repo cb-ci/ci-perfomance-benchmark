77423f1079338418416a64e8fc2399cb4ff2317e
cccaternberg <acaternberg@cloudbees.com>
2025-11-11 14:50:38 +0100

    add

--- scripts/triggerSimplePipeline.sh
+++ scripts/triggerSimplePipeline.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-#source ../set-env.sh
+source ./set-env.sh
 #TOKEN="$JENKINS_USERNAME:$JENKINS_API_TOKEN"
 #curl -u "$JENKINS_API_TOKEN" -X POST "$JENKINS_HOST/$JENKINS_JOB_PATH"
 
 
-JENKINS=https://jenkins.example
-USER=svc
-TOKEN=xxx
-JOB='perf/LoadJob'
+
 
 #CRUMB=$(curl -s -u "$USER:$TOKEN" "$JENKINS/crumbIssuer/api/json" | jq -r .crumb)
 #curl -H "Jenkins-Crumb:$CRUMB" ....
 
-for i in {1..10}; do
-  curl -s -u "$USER:$TOKEN" \
-    -X POST "$JENKINS/job/$JOB/buildWithParameters?PROFILE=medium&cause=perf"
+for i in {1..5}; do
+  echo "trigger build $i"
+  curl -s -u "$JENKINS_API_TOKEN" \
+    -X POST "$JENKINS_HOST/$JENKINS_JOB_PATH"
+    #-X POST "$JENKINS/job/$JOB/buildWithParameters?PROFILE=medium&cause=perf"
+    sleep 2
 done
